41d251609b72936aec63fcd3535fa7bf44149284
Lowie <lowie.v.e@gmx.com>
2022-03-26 20:34:48 +0100

    Add files via upload

--- sh/cmus-get-song.sh
+++ sh/cmus-get-song.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-[[ $(echo "$(cmus-remote -Q)" | wc -l) == 1 ]] >/dev/null 2>&1 || cmus-remote -Q | grep title | awk {'print $3'}
+[[ $(echo "$(cmus-remote -Q)" | wc -l) == 1 ]] >/dev/null 2>&1 || echo "MUS: $(cmus-remote -Q | grep title | cut -d' ' -f3-) | "
